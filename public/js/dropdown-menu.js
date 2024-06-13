056026f5eb2aa0a88bae4df4eecc870b244421ad
Valeriy Petrov <piswasser38@gmail.com>
2024-06-13 21:51:43 +0800

    timeout added

--- public/js/dropdown-menu.js
+++ public/js/dropdown-menu.js
@@ -1,8 +1,18 @@
+let timer;
+let canRun = true;
+function toggleCanRun () {
+    canRun = true;
+    clearTimeout(timer);
+};
 $( ".dropdown-hover , .dropdown-list" ).hover(function() {
+    if (canRun === true) {
+        canRun = false;
+        timer = setTimeout(toggleCanRun, 2000);
+    }
     $(".dropdown-list").addClass( "show" );
-    console.log("yes")
   }, function() {
-    $(".dropdown-list").removeClass( "show" );
-    console.log("no")
+    if (canRun === true) {
+        $(".dropdown-list").removeClass( "show" );
+    }
   }
 )
