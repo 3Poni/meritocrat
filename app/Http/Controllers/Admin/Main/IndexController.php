0c603bd38e2cabe0a1792db18724ebb1c5ed289f
Valeriy Petrov <piswasser38@gmail.com>
2024-06-16 13:51:58 +0800

    admin panel added

---
+++ app/Http/Controllers/Admin/Main/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Main;
+
+use App\Http\Controllers\Controller;
+use App\Models\Articles;
+use App\Models\Category;
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+
+    public function __invoke(Request $request)
+    {
+        $data = [];
+        $data ['usersCount'] = User::all()->count();
+        return view('admin.main.index', compact('data'));
+    }
+}
