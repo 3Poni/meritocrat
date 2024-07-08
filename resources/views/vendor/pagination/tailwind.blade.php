14bec2d7a4a2f9d1afd848188d352164bb8cfd3b
Valeriy Petrov <piswasser38@gmail.com>
2024-07-08 23:17:27 +0800

    custom pagination added

---
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -0,0 +1,55 @@
+@if ($paginator->hasPages())
+    <div role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="pagination">
+            <div class="pagination-items">
+                    {{-- Previous Page Link --}}
+                    @if ($paginator->onFirstPage())
+                        <span aria-disabled="true" aria-label="{{ __('pagination.previous') }}">
+                            <span class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default rounded-l-md leading-5 dark:bg-gray-800 dark:border-gray-600" aria-hidden="true">
+                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                                </svg>
+                            </span>
+                        </span>
+                    @else
+                        <a href="{{ $paginator->previousPageUrl() }}">
+                            <div class="arrow-previous">
+                                <img src="../img/icons/left-arrow.png" alt="">
+                            </div>
+                        </a>
+                    @endif
+
+                    {{-- Pagination Elements --}}
+                    @foreach ($elements as $element)
+                        {{-- "Three Dots" Separator --}}
+                        @if (is_string($element))
+                            <input type="button" value="0{{ $element }}">
+                        @endif
+
+                        {{-- Array Of Links --}}
+                        @if (is_array($element))
+                            @foreach ($element as $page => $url)
+                                @if ($page == $paginator->currentPage())
+                                    <a href="{{ $url }}">
+                                    <input type="button" class="active" value="0{{ $page }}">
+                                    </a>
+                                @else
+                                <a href="{{ $url }}">
+                                <input type="button" value="0{{ $page }}">
+                                </a>
+                                @endif
+                            @endforeach
+                        @endif
+                    @endforeach
+
+                    {{-- Next Page Link --}}
+                    @if ($paginator->hasMorePages())
+                        <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150 dark:bg-gray-800 dark:border-gray-600 dark:active:bg-gray-700 dark:focus:border-blue-800" aria-label="{{ __('pagination.next') }}">
+                            <div class="arrow-next">
+                                <img src="../img/icons/right-arrow.png" alt="">
+                            </div>
+                        </a>
+                    @else
+                    @endif
+            </div>
+    </div>
+@endif
