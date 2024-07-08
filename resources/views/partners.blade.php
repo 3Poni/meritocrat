9d89c6fd093049d5651d78d8812a4490e466684d
Valeriy Petrov <piswasser38@gmail.com>
2024-07-08 23:21:31 +0800

    dynamic content and pagination added

--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -18,34 +18,16 @@
         <h1 class="section-h1">Партнеры</h1>
         <div class="partners-buttons">
             <button class="partners-button">Все</button>
-            <button class="partners-button">Бухгалтерия</button>
-            <button class="partners-button">Расчетный счет</button>
-            <button class="partners-button">Продажи и маркетинг</button>
-            <button class="partners-button">Телефония</button>
-            <button class="partners-button">Подбор персонала</button>
-            <button class="partners-button">Юриспруденция</button>
-            <button class="partners-button">CRM/ERP</button>
-            <button class="partners-button">Онлайн-касса и эквайринг</button>
-            <button class="partners-button">Маркировка</button>
+            @foreach($tags as $tag)
+            <button class="partners-button">{{ $tag->title }}</button>
+            @endforeach
         </div>
         <div class="partners-items">
-            <img src="../img/out-slid-3.png" alt="">
-            <img src="../img/out-slid.png" alt="">
-            <img src="../img/out-slid-2.png" alt="">
-            <img src="../img/out-slid-5.png" alt="">
-            <img src="../img/out-slid-4.png" alt="">
-            <img src="../img/out-slid-3.png" alt="">
+            @foreach($partners as $partner)
+            <img src="{{ $partner->img }}" alt="">
+            @endforeach
         </div>
-        <div class="pagination">
-            <div class="pagination-items">
-                <input type="button" value="01" class="active">
-                <input type="button" value="02">
-                <input type="button" value="03">
-                <div class="arrow-next">
-                    <img src="../img/icons/right-arrow.png" alt="">
-                </div>
-            </div>
-        </div>
+        {{ $partners->links() }}
     </div>
 </section>
 @endsection
