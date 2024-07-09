c38cb88fe2185cd0c15f4c33d2d388416b431438
Valeriy Petrov <piswasser38@gmail.com>
2024-07-09 23:56:27 +0800

    routes changed

--- resources/views/admin/main/index.blade.php
+++ resources/views/admin/main/index.blade.php
@@ -48,6 +48,19 @@
                         <!-- small box -->
                         <div class="small-box bg-success">
                             <div class="inner">
+                                <h3>{{ $data['projectCount'] }}</h3>
+                                <p>Проекты</p>
+                            </div>
+                            <div class="icon">
+                                <i class="nav-icon fas fa-tags"></i>
+                            </div>
+                            <a href="{{ route('admin.project.index') }}" class="small-box-footer">Подробнее <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-success">
+                            <div class="inner">
                                 <h3>{{ $data['partnerCount'] }}</h3>
                                 <p>Партнеры</p>
                             </div>
