6112865c3e2372f88b15a1dab71571d9fe416b1f
Valeriy Petrov <piswasser38@gmail.com>
2024-07-15 23:11:19 +0800

    ajax request added

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -23,12 +23,14 @@
             <div class="container">
                 <h1 class="projects_h1">Проекты</h1>
                 <div class="projects-row_items_categories">
-                    <div class="project-item">
+                    <div class="project-item" style="margin-right: 14px">
                         <input type="button" class="project-categories_button active" value="Все">
                     </div>
                     @foreach($services as $service)
                         <div class="project-item">
-                            <input type="button" class="project-categories_button" value="{{ $service->title }}">
+                            <input type="button"  onclick="selectService({{ $service->id }})" class=" {{ in_array($service->id, $service_ids) ? 'active' : '' }} project-categories_button"
+                                   id="service_{{$service->id}}"
+                                   value="{{ $service->title }}">
                         </div>
                     @endforeach
                 </div>
@@ -51,9 +53,9 @@
                     @foreach($projects as $project)
                         <div class="project-slider-item">
                             <div class="project-slider-item_img">
-                                <img src="{{ $project->img }}" alt="">
+                                <img src="{{ $project->img }}" alt="проект">
                             </div>
-                            <div class="project-slider-item_text" style="margin-top: -10px;display: flex;min-height: 192px;flex-direction: column;justify-content: space-between;">
+                            <div class="project-slider-item_text">
                                 <h1>{{ $project->header }}</h1>
                                 <p>{{ $project->description }}</p>
                                 <span>{{ $project->service->title }}</span>
@@ -67,5 +69,91 @@
     </section>
 @endsection
 @push('script')
+    <script>
+        var initUrl = '';
+        let selectedIds = [];
+        let myTimer = false;
+
+        function selectService(id) {
+            myTimer = true;
+            let clicked = $('#service_'+id)
+            let allSelected = $('.project-categories_button')
+            if(clicked.hasClass('active')) {
+                clicked.toggleClass('active');
+                const index = selectedIds.indexOf(id);
+                if (index > -1) { // only splice array when item is found
+                    selectedIds.splice(index, 1); // 2nd parameter means remove one item only
+                }
+                initUrl = initUrl.includes(',') ? initUrl.split('=')[0] + '=' + selectedIds.join(',') : initUrl.split('?')[0]
+                console.log(initUrl)
+            }else{
+                selectedIds.push(id);
+                clicked.toggleClass('active')
+                initUrl = initUrl.includes('service_id') ? initUrl + ',' + id : initUrl + '?service_id=' + id
+                console.log(initUrl)
+            }
+            setTimeout(redirect, 200)
+            //
+        }
+
+        function redirect() {
+            if(myTimer) {
+                myTimer = false;
+                setTimeout(redirect, 200)
+            }else{
+                getData(initUrl)
+            }
+
+        }
+
+        function getData(url) {
+            $.ajax({
+                url: "/api/projects?" + url,
+                type:"GET",
+                data:{
+                    "_token": "{{ csrf_token() }}",
+                },
+                success:function(response){
+                    let projectsRendered = '';
+                    let linksRendered = '';
+                    let projectsPoint = $('.projects-items');
+                    let paginationPoint = $('.pagination-items');
+                    if(response.result.data.length) {
+                        response.result.data.forEach(function (item) {
+                            projectsRendered += `<div class="project-slider-item">
+                            <div class="project-slider-item_img">
+                                <img src="${item.img}" alt="проект">
+                            </div>
+                            <div class="project-slider-item_text">
+                                <h1>${item.header}</h1>
+                                <p>${item.description}</p>
+                                <span>${item.service.title}</span>
+                            </div>
+                        </div>`
+                        })
+                        response.result.links.forEach(function (item) {
+                            if(item.url !== null) {
+                                if(item.label === "Next &raquo;") {
+                                    linksRendered += `<a href="${item.url.replace('/api', '')}" rel="next" aria-label="Next &amp;raquo;">
+                                        <div class="arrow-next">
+                                            <img src="../img/icons/right-arrow.png" alt="">
+                                        </div>
+                                    </a>`
+                                }else{
+                                    linksRendered += `<a href="${item.url.replace('/api', '')}">
+                                    <input type="button" class="${item.active ? 'active' : ''}" value="0${Number(item.label)}">
+                                </a>`
+                                }
+
+                            }
+
+                        })
+                    }
+                    projectsPoint.html(projectsRendered)
+                    paginationPoint.html(linksRendered)
+                },
+            });
+        }
+    </script>
 <script src="{{ asset('js/dropdown-projects.js') }}"></script>
 @endpush
