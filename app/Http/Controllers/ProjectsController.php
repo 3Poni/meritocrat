55751ef5dba9ccbad7f838deaeba15ac2cadb5cc
Valeriy Petrov <piswasser38@gmail.com>
2024-07-15 23:10:09 +0800

    pag links fix

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -11,8 +11,9 @@ class ProjectsController extends Controller
     public function __invoke(Project $project)
     {
         $data = $project::with('service');
-        $projects = $data->filter(request(['service_id']))->paginate(6);
+        $projects = $data->filter(request(['service_id']))->paginate(6)->appends(request(['service_id']));
+        $service_ids = !empty(request(['service_id'])) ? explode(',',request(['service_id'])['service_id']) : [];
         $services = $data->getRelation('service')->get();
-        return view ('projects', compact('projects', 'services'));
+        return view ('projects', compact('projects', 'services', 'service_ids'));
     }
 }
